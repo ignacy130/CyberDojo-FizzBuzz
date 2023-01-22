569a681f7e16d7d0a0117a869e93c682d52f76cc
r5WTh5 <r5WTh5@cyber-dojo.org>
2023-01-22 10:36:30 +0000

    8 ran tests, no prediction, got red

--- files/FizzBuzz.cs
+++ files/FizzBuzz.cs
@@ -5,6 +5,12 @@ public class FizzBuzz
         if(input%3==0&&input%5==0){
             return "FizzBuzz";
         }
+        if(input%3==0){
+            return "Fizz";
+        }
+        if(input%5==0){
+            return "Buzz";
+        }
         return input.ToString();
     }
 }
