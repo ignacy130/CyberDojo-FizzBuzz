f94596137692d960b752232b3888e230df634303
r5WTh5 <r5WTh5@cyber-dojo.org>
2023-01-22 10:36:07 +0000

    7 ran tests, no prediction, got red

--- files/FizzBuzz.cs
+++ files/FizzBuzz.cs
@@ -2,6 +2,9 @@ public class FizzBuzz
 {
     public static string Answer(int input)
     {
+        if(input%3==0&&input%5==0){
+            return "FizzBuzz";
+        }
         return input.ToString();
     }
 }
